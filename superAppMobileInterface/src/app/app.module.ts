93cf3fb2e1c778fbb0e12fea73813a886731a29f
vitor-gercov <gercovvitor@gmail.com>
2021-03-28 15:36:24 -0300

    primeira pagina criada

--- superAppMobileInterface/src/app/app.module.ts
+++ superAppMobileInterface/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { ErrorHandler, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AnimesPage } from '../pages/animes/animes';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,17 +14,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AnimesPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    AnimesPage
   ],
   providers: [
     StatusBar,
---
+++ superAppMobileInterface/src/pages/animes/animes.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-animes',
+  templateUrl: 'animes.html',
+})
+export class AnimesPage {
+
+  constructor(public navCtrl: NavController) {
+  
+  }
+  
+}
