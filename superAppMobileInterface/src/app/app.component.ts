1401fa9017249e49f174e5cb11a6b6606af79e9d
Renato Vieira <renato@getconnect.com.br>
2021-03-28 18:26:06 -0300

    Merge

--- superAppMobileInterface/src/app/app.component.ts
+++ superAppMobileInterface/src/app/app.component.ts
@@ -1,51 +1,18 @@
-<<<<<<< HEAD
 import { Component, ViewChild } from "@angular/core";
 import { Nav, Platform } from "ionic-angular";
-import { StatusBar, StatusBarOriginal } from "@ionic-native/status-bar";
-import {
-  SplashScreen,
-  SplashScreenOriginal,
-} from "@ionic-native/splash-screen";
-=======
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { AnimesPage } from '../pages/animes/animes';
-import { GamesPage } from '../pages/games/games';
-import { SpacexPage } from '../pages/spacex/spacex';
-import { MusicPage } from '../pages/music/music';
->>>>>>> 5a5248a731ff2a6ede43ccdf826ddce40901c138
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
 @Component({
   templateUrl: "app.html",
 })
 export class MyApp {
-<<<<<<< HEAD
   constructor(
     public platform: Platform,
-    public statusBar: StatusBarOriginal,
-    public splashScreen: SplashScreenOriginal
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
   ) {
     this.initializeApp();
-=======
-  @ViewChild(Nav) nav: Nav;
-
-  rootPage: any = GamesPage;
-
-  pages: Array<{title: string, component: any}>;
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Animes', component: AnimesPage },
-      { title: 'Games', component: GamesPage },
-      { title: 'SpaceX', component: SpacexPage },
-      { title: 'Músicas', component: MusicPage }
-    ];
->>>>>>> 5a5248a731ff2a6ede43ccdf826ddce40901c138
   }
 
   initializeApp() {
@@ -54,11 +21,4 @@ export class MyApp {
       this.splashScreen.hide();
     });
   }
-<<<<<<< HEAD
-=======
-
-  openPage(page) {
-    this.nav.setRoot(page.component);
-  }
->>>>>>> 5a5248a731ff2a6ede43ccdf826ddce40901c138
 }
